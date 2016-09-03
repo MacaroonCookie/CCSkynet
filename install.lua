c0e6a8dbc116f025435b6a41ebd5bcc459caac8b
Seth Cook <sethcook@purdue.edu>
2016-09-03 17:03:31 -0400

    Adding install script for reference.

---
+++ install.lua
@@ -0,0 +1,20 @@
+--[[ OS Installation Script
+--   Scientia potentia est
+--]]
+
+local system_files = {
+    '/' = {
+        'apis/'={},
+        'config/'={},
+        'programs/'={},
+        'system/'={
+            'boot/'={}
+        },
+        'tmp/'={},
+        'users/'={
+            'root/'={}
+        },
+        'var/'={}
+    }
+}
+
